4816349b5c265be8daff81fe4a3ee304b25afd4e
julien.cav <julien.cav@gmail.com>
2017-04-28 15:20:05 +0200

    change on siminta.js

--- static/scripts/siminta.js
+++ static/scripts/siminta.js
@@ -28,16 +28,16 @@ $(function () {
         x.send();
     };
 
-    load(20);
-    load(100);
-    load(500);
-    load(2000);
-    load(3000);
-    setTimeout(function () {
-        Pace.ignore(function () {
-            load(3100);
-        });
-    }, 4000);
+    //load(20);
+    // load(100);
+    // load(500);
+    // load(2000);
+    // load(3000);
+    // setTimeout(function () {
+    //     Pace.ignore(function () {
+    //         load(3100);
+    //     });
+    // }, 4000);
 
     Pace.on('hide', function () {
         console.log('done');
