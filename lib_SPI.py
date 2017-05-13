b9a017bc23b64db35866cef34d2dda72e6264371
julien <julien.cav@gmail.com>
2017-05-13 13:16:26 +0000

    add lib_SPI

---
+++ lib_SPI.py
@@ -0,0 +1,35 @@
+import spidev
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.mode = 0b00
+
+def bat_route():
+    res = spi.xfer([0x04, 0x00])
+    print("bat_route = " + str(res[0]))
+    return(res)
+
+def bat_aux():
+    res = spi.xfer([0x04, 0x00])
+    print("bat_aux = " + str(res[0]))
+    return(res)
+
+def hum_int():
+    res = spi.xfer([0x05, 0x00])
+    print("hum_int = " + str(res[0]))
+    return(res)
+
+def hum_ext():
+    res = spi.xfer([0x06, 0x00])
+    print("hum_ext = " + str(res[0]))
+    return(res)
+
+def temp_int():
+    res = spi.xfer([0x07, 0x00, 0x00, 0x00, 0x00])
+    print("temp_int = " + str(res[0]))
+    return(res)
+
+def temp_ext():
+    res = spi.xfer([0x08, 0x00])
+    print("temp_ext = " + str(res[0]))
+    return(res)
--- lib_picture.py
+++ lib_picture.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 
 ''' LIB TO TAKE PICTURE '''
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
 from subprocess import call
 import time, platform 
 SYSTEM = platform.system()
+fromaddr = "poulettemylove@gmail.com"
+toaddr = "julien.cav@gmail.com"
+msg = MIMEMultipart()
 
 PATH = "static/img/"
 
@@ -22,13 +29,27 @@ def take_pic():
 
 
 def send_mail_pic(path):
-    #filename = "IMG_Poulette"+str(line-1)+".jpg"
-    #PATH = "../gallery/"
-    fullpath = path
-    call(["mutt", "-s", "Poulette Picture", "-a", fullpath, "--", "julien.cav@gmail.com"])
-    #call(["mutt -s 'Ma voiture' -a *.jpg -- julien.cav@gmail.com < mon_message.txt"])
-    print("La photo est envoyé à l'adresse julien.cav@gmail.com")
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = "Photo send from Poulette Life System !"
+	body = "Hello c'est poulette ! voici la photo"
+	msg.attach(MIMEText(body, 'plain'))
+	filename = "00011_photo.jpg"
+	attachment = open(path, "rb")
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload((attachment).read())
+	encoders.encode_base64(part)
+	part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+	msg.attach(part) 
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(fromaddr, "superpoulette")
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
 
 if __name__ == '__main__':
-    RES = take_pic()
+    RES = send_mail_pic("static/img/00011_photo.jpg")
     print(RES)
+
+
