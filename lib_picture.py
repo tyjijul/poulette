dc45db26f4835ae0ee547dffb30b32e5b3b6a3bf
julien.cav <julien.cav@gmail.com>
2017-06-17 00:10:09 +0200

    add email addr

--- lib_picture.py
+++ lib_picture.py
@@ -15,8 +15,8 @@ from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
 
-fromaddr = "YOUR EMAIL"
-toaddr = "EMAIL ADDRESS YOU SEND TO"
+fromaddr = "poulettemylove@gmail.com"
+
 
 msg = MIMEMultipart()
 
